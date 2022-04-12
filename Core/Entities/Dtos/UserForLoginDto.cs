2dc6c917a1618304cac9e277646d78584a17494b
PhantomHelios <ikrhn35@outlook.com>
2022-04-13 00:57:36 +0300

    JWT

--- Core/Entities/Dtos/UserForLoginDto.cs
+++ Core/Entities/Dtos/UserForLoginDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Entities.Dtos
+namespace Core.Entities.DTOs
 {
     public class UserForLoginDto : IDto
     {
--- Core/Entities/Dtos/UserForRegisterDto.cs
+++ Core/Entities/Dtos/UserForRegisterDto.cs
@@ -1,10 +1,16 @@
-﻿namespace Core.Entities.Dtos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.DTOs
 {
     public class UserForRegisterDto : IDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
